023fab6ac92a65e1be4bf43f1ecb658f9cdf0d27
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-05 12:03:19 +0200

    Hit API and filter the data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 const getWeatherData = (location) => {
-  const apiKey = '8dbe756ea48f4c0da2395345241605';
-  const p = fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
-    { mode: 'cors' },
-  );
-  p.then((response) => {
-    return response.json();
-  }).then((data) => {
-    console.log(data.current);
+  return new Promise((resolve, reject) => {
+    const apiKey = '8dbe756ea48f4c0da2395345241605';
+    const p = fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
+      { mode: 'cors' },
+    );
+
+    p.then((response) => {
+      return response.json();
+    }).then((data) => {
+      resolve(data);
+    });
   });
 };
 
-getWeatherData('durban');
+const processWeatherData = (data) => {
+  console.log(data);
+  return {
+    condition: data.current.condition.text,
+    icon: data.current.condition.icon,
+    temp_c: data.current.temp_c,
+    temp_f: data.current.temp_f,
+  };
+};
+
+(async () => {
+  const data = await getWeatherData('durban');
+  const filteredData = processWeatherData(data);
+  console.log(filteredData);
+})();
