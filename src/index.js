d882afd5dcf1a323d75b82e7e6a0f0d1b0a220a6
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-05 09:07:01 +0200

    Refactor getWeatherData

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const getWeatherData = (location) => {
-  // const apiKey = '6654ca42d2499aca01507f539a69b74383c5c9d7';
-  // const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=durban`;
+  const apiKey = '8dbe756ea48f4c0da2395345241605';
   const p = fetch(
-    'https://api.weatherapi.com/v1/current.json?key=8dbe756ea48f4c0da2395345241605&q=durban',
+    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
     { mode: 'cors' },
   );
   p.then((response) => {
@@ -12,4 +11,4 @@ const getWeatherData = (location) => {
   });
 };
 
-getWeatherData('Durban');
+getWeatherData('durban');
